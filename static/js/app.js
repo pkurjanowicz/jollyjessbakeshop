a3383a58df82e068e3957611c2a3e884a7332a8e
Peter <pkurjanowicz5@gmail.com>
2019-08-31 22:23:30 -0600

    made slide over menu

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,15 @@
 
 
 
-brownieItem = (queryValue) => {
-    const queryValue = urlParams.get('item');
-    return location.href = `/?item=${queryValue}`;
+// brownieItem = (queryValue) => {
+//     const queryValue = urlParams.get('item');
+//     return location.href = `/?item=${queryValue}`;
+// }
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
 }
